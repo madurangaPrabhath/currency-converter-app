874c45e054384de4af1bdf53cef148f72a3c0b80
maduracourse <maduracourse@gmail.com>
2025-12-15 02:17:44 +0530

    Update allowedOrigins to include Vercel frontend URL for CORS configuration

--- server/src/index.js
+++ server/src/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 const allowedOrigins = [
   "http://localhost:3000",
-  process.env.FRONTEND_URL,
+  "https://currency-converter-website-app.vercel.app",
 ];
 
 app.use(
